7e48b3ede620f5107910fdddbe2b42687718aa76
Michael Berg <michaelberg@bmgrobotics.com>
2021-02-07 18:16:14 -0700

    fixed the bugs

--- Knapsack/kernel.cu
+++ Knapsack/kernel.cu
@@ -8,45 +8,55 @@
 #include <cstdint>
 #include <iostream>
 #include <fstream>
+#include <chrono>
+#include <atomic>
+
 
 __global__ void knapKernel(
-    const uint16_t* maxWeight, const uint16_t* arraySize, 
-    const uint16_t* valueD, const uint16_t* weightD, 
-    unsigned long long int* victorId, uint16_t* victorValue, 
-    unsigned long long int* offset)
+    const uint16_t* maxWeight, const uint16_t* arraySize,
+    const uint16_t* valueD, const uint16_t* weightD,
+    unsigned long long int* victorId, uint16_t* victorValue,
+    unsigned long long int* offset, int* memSize, unsigned long long int runLength)
 {
-    int id = blockIdx.x * blockDim.x + threadIdx.x;
-    unsigned long long int trueId = id + *offset;
+    unsigned long long int threadId = blockIdx.x * blockDim.x + threadIdx.x;
+    unsigned int id = threadId + *offset;
     unsigned int sackWeight = 0;
-    
-    //check validity
-    for (int a1 = 0; trueId; a1++) {
-        if (trueId & 1) {
-            sackWeight += (weightD[a1]);
-        }
-        trueId >>= 1;
-    }
-    //check against memory
-    trueId = id + *offset;
 
-    unsigned int sackValue = 0;
-    if (sackWeight <= *maxWeight ) {
-        for (int a1 = 0; trueId; a1++) {
-            if (trueId & 1) {
-                sackValue += (valueD[a1]);
-            }
-            trueId >>= 1;
+
+    if (id <= runLength) {
+        if (*offset == 0) {
+            victorValue[threadId] = 0;
+            victorId[threadId] = 0;
         }
-        if (sackValue > victorValue[id]) {
-            victorValue[id] = sackValue;
-            victorId[id] = id + *offset;
+        //check validity
+        unsigned long long int trueIdLoc = id;
+        for (int a1 = 0; trueIdLoc; a1++) {
+            if (trueIdLoc & 1) {
+                sackWeight += (weightD[a1]);
+            }
+            trueIdLoc >>= 1;
+        }
+
+        //check against memory
+        unsigned int sackValue = 0;
+        if (sackWeight <= *maxWeight) {
+            trueIdLoc = id;
+            for (int a1 = 0; trueIdLoc; a1++) {
+                if (trueIdLoc & 1) {
+                    sackValue += (valueD[a1]);
+                }
+                trueIdLoc >>= 1;
+            }
+            if (sackValue > victorValue[threadId]) {
+                victorValue[threadId] = sackValue;
+                victorId[threadId] = id;
+            }
         }
     }
 }
 
 int main()
 {
-
     uint16_t arraySize = 0;
     uint16_t maxWeight = 0;
 
@@ -106,6 +116,8 @@ int main()
     cudaMalloc(&arraySizeD, sizeof(uint16_t));
     unsigned long long int* offsetD;
     cudaMalloc(&offsetD, sizeof(unsigned long long int));
+    int* memSizeD;
+    cudaMalloc(&memSizeD, sizeof(int));
 
     cudaMemcpy(weightD, weightH, size, cudaMemcpyHostToDevice);
     cudaMemcpy(valueD, valueH, size, cudaMemcpyHostToDevice);
@@ -115,41 +127,77 @@ int main()
     cudaDeviceProp deviceProp;
     cudaGetDeviceProperties(&deviceProp, 0); // 0-th device
 
+    //determin block size
     int blockSize = deviceProp.maxThreadsPerMultiProcessor / deviceProp.maxBlocksPerMultiProcessor;
-    unsigned long long int numThreads = pow(2.0, arraySize);
-    int numBlocks = deviceProp.maxBlocksPerMultiProcessor * deviceProp.multiProcessorCount;
+    unsigned long long int runLength = pow(2.0, arraySize);
+
+    
+
+
+
+    /// This will launch a grid that can maximally fill the GPU, on the default stream with kernel arguments
+    int numBlocksPerSm = 0;
+    // Number of threads my_kernel will be launched with
+    int numThreads = 128;
+    cudaOccupancyMaxActiveBlocksPerMultiprocessor(&numBlocksPerSm, knapKernel, numThreads, 0);
+
+
+    //determin grid size
+    int memSize = numBlocksPerSm * deviceProp.multiProcessorCount * numThreads;
 
     uint16_t* victorValueD;
-    cudaMalloc(&victorValueD, sizeof(uint16_t) * numBlocks * deviceProp.maxThreadsPerBlock);
+    cudaMalloc(&victorValueD, sizeof(uint16_t) * memSize);
     unsigned long long int* victorIdD;
-    cudaMalloc(&victorIdD, sizeof(unsigned long long int) * numBlocks * deviceProp.maxThreadsPerBlock);
-
-    //capable up to n=50 15 minutes run time 
-    //the cpu finish the computation meaning that under ~ n=21 all computation is done by the cpu
-    std::cout << numThreads << std::endl;
-    for (unsigned long long int a1 = 0; a1 < numThreads; a1 += numBlocks * deviceProp.maxThreadsPerBlock) {
-        
-        cudaMemcpy(offsetD, &a1, sizeof(unsigned long long int), cudaMemcpyHostToDevice);
-        knapKernel <<<numBlocks, blockSize >>> (maxWeightD, arraySizeD, valueD, weightD, victorIdD, victorValueD, offsetD);
-    }
+    cudaMalloc(&victorIdD, sizeof(unsigned long long int) * memSize);
+    //copy number of threads
     
+    //copy memory size
+    cudaMemcpy(memSizeD, &memSize, sizeof(int), cudaMemcpyHostToDevice);
+
+    dim3 dimBlock(numThreads, 1, 1);
+    dim3 dimGrid(deviceProp.multiProcessorCount * numBlocksPerSm, 1, 1);
+
+    int incriment = deviceProp.multiProcessorCount * numBlocksPerSm * numThreads;
+
+    for (unsigned long long int offset = 0; offset < runLength; offset += incriment) {
+        cudaDeviceSynchronize();
+        cudaMemcpy(offsetD, &offset, sizeof(unsigned long long int), cudaMemcpyHostToDevice);
+        knapKernel <<<dimGrid, dimBlock >>> (maxWeightD, arraySizeD, valueD, weightD, victorIdD, victorValueD, offsetD, memSizeD, runLength);
+    }
+
     //create host memory
-    unsigned long long int*victorIdH = (unsigned long long int*)malloc (sizeof(unsigned long long int) * (numBlocks * deviceProp.maxThreadsPerBlock));
-    uint16_t *victorValueH = (uint16_t*)malloc(sizeof(uint16_t) * (numBlocks * deviceProp.maxThreadsPerBlock));
+    unsigned long long int *victorIdH = (unsigned long long int*)malloc (sizeof(unsigned long long int) * memSize);
+    uint16_t *victorValueH = (uint16_t*)malloc(sizeof(uint16_t) * memSize);
 
-    cudaMemcpy(victorIdH   , victorIdD,    sizeof(unsigned long long int) * numBlocks * deviceProp.maxThreadsPerBlock, cudaMemcpyDeviceToHost);
-    cudaMemcpy(victorValueH, victorValueD, sizeof(uint16_t) * numBlocks * deviceProp.maxThreadsPerBlock, cudaMemcpyDeviceToHost);
+    //initialize to zero
+    for (int a1 = 0; a1 < memSize; a1++) {
+        victorIdH[a1] = 0;
+        victorValueH[a1] = 0;
+    }
 
-    int highValue = 0;
+    cudaMemcpy(victorIdH, victorIdD, sizeof(unsigned long long int) * memSize, cudaMemcpyDeviceToHost);
+    cudaMemcpy(victorValueH, victorValueD, sizeof(uint16_t) * memSize, cudaMemcpyDeviceToHost);
+
+    //for debugging what is in memory
+    /*for (int a1 = 0; a1 < memSize; a1++) {
+        if (victorIdH[a1] != 0) {
+            std::cout << a1 << ": " << victorIdH[a1] << ", " << victorValueH[a1] << std::endl;
+        }
+        
+    }*/
+
+    uint16_t highValue = 0;
     unsigned long long int highId = 0;
 
-    for (int a1 = 0; a1 < numBlocks * deviceProp.maxThreadsPerBlock; a1++) {
+    for (int a1 = 0; a1 < memSize; a1++) {
         if (victorValueH[a1] > highValue) {
             highValue = victorValueH[a1];
             highId = victorIdH[a1];
         }
     }
 
+    std::cout << "Sets to Search:" << runLength << std::endl;
+    std::cout << "Mem Size:" << memSize << std::endl;
     std::cout << "Value:" << highValue << " ID:" << highId << std::endl;
     for (int a1 = 0; highId; a1 ++) {
         if (highId & 1)
@@ -165,8 +213,8 @@ int main()
     cudaFree(arraySizeD);
     cudaFree(offsetD);
 
-    //delete victorIdH;
-    //delete victorValueH;
+    free(victorIdH);
+    free(victorValueH);
 
 
     // cudaDeviceReset must be called before exiting in order for profiling and
